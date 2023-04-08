b0cec5708aa29029d3f6cd08db39c70f59a85645
sahil <sahilvichare81@gmail.com>
2023-04-08 22:20:14 +0530

    Merged the frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import { Page1 } from './components/Page1';
+import  Cards from './components/Cards'
+import Title from './components/Title';
+import Ellipse from './components/Ellipse';
+import Todolist from './components/Todolist';
 
 function App() {
   return (
     <div className="container">
       <Page1/>
+      <Todolist/>
+      <Cards/>
+      <Title/>
+      <Ellipse/>
     </div>
     
   );
